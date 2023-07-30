4b382838a17da1ca2ea2a3ef9728403ea359e3fc
Heather <>
2023-07-30 18:54:54 -0500

    Challenge 4 Complete

---
+++ Challenge 4.js
@@ -0,0 +1,25 @@
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. 
+// For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+
+function addUp(num){
+    
+    addedNum = 0
+    for (let i = 0; i <= num; i++) {
+        addedNum = addedNum + 1;
+        return addedNum
+      }
+}
+
+
+console.log(addUp(4))
+
+console.log(addUp(13))
+
+console.log(addUp(600))
